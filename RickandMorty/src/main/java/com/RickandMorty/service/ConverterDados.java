130e97a189de5097ea323995657cec3547ca5cbe
ze <jvitinhosa@gmail.com>
2025-10-24 08:52:44 -0300

    .

--- RickandMorty/src/main/java/com/RickandMorty/service/ConverterDados.java
+++ RickandMorty/src/main/java/com/RickandMorty/service/ConverterDados.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConverterDados {
 
-    private final ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper(); // Objeto para converter JSON em objeto Java
 
     public <T> T obterDados(String json, Class<T> classe) {
         try {
+            // Converte o JSON recebido em um objeto da classe desejada
             return mapper.readValue(json, classe);
         } catch (Exception e) {
+            // Lança exceção caso a conversão falhe
             throw new RuntimeException("Erro ao converter JSON: " + e.getMessage());
         }
     }
+
 }
